51b9f119bd830a8be4239323866197fe841a90cf
jhong92 <hjsky1010@gmail.com>
2024-01-29 02:43:20 +0900

    Lab 1: MapReduce - coordinator.go

--- src/mr/coordinator.go
+++ src/mr/coordinator.go
@@ -5,24 +5,75 @@ import "net"
 import "os"
 import "net/rpc"
 import "net/http"
+import "strconv"
+import "sync"
 
 
 type Coordinator struct {
 	// Your definitions here.
-
+	mu sync.Mutex
+	mapTasks []Task
+	reduceTasks []Task
+	mapDone bool
+	reduceDone bool
 }
 
 // Your code here -- RPC handlers for the worker to call.
+
+func (c *Coordinator) AssignTask(replyTask *Task) error {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	replyTask.TaskType = BREAK
+	if !c.mapDone {
+		replyTask.TaskType = MAP
+	}
+	else if !c.reduceDone(){
+		replyTask.TaskType = REDUCE
+	}
+	AssignTask(replyTask)
+	return nil
+}
+
+// type Task struct {
+// 	TaskStatus TaskStatus
+// 	TaskType TaskType
+// 	InputFiles []string
+// 	Index int
+// 	TimeStamp time.Time
+// }
+
+func (c *Coordinator) AssignTask(replyTask *Task){
+	if replyTask.TaskType==BREAK{
+		return nil
+	}
+	var refTasks []Task
+	if replyTask.TaskType==MAP{
+		refTasks = c.mapTasks
+	}
+	else{ // in case of REDUCE
+		refTasks = c.reduceTasks
+	}
+	for index, task := range refTasks {
+		tenSecondsAgo := time.Now().Add(-10 * time.Second)
+		if task.TaskStatus==NOT_STARTED || task.TaskStatus==(WAITING && task.TimeStamp.Before(tenSecondsAgo)){
+			refTasks[index].TaskStatus = WAITING
+			refTasks[index].TimeStamp = time.Now()
+			replyTask.InputFiles = task.InputFiles
+			replyTask.Index = index
+			break
+		}
+	}
+}
 
 //
 // an example RPC handler.
 //
 // the RPC argument and reply types are defined in rpc.go.
 //
-func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
-	reply.Y = args.X + 1
-	return nil
-}
+// func (c *Coordinator) Example(args *ExampleArgs, reply *ExampleReply) error {
+// 	reply.Y = args.X + 1
+// 	return nil
+// }
 
 
 //
@@ -63,8 +114,44 @@ func MakeCoordinator(files []string, nReduce int) *Coordinator {
 	c := Coordinator{}
 
 	// Your code here.
+	c.mapDone = false
+	c.reduceDone = false
 
+	var mapTasks []Task
+	for i:=0; i<len(files); i++{
+		var task = Task{
+			TaskType: MAP,
+			TaskStatus: NOT_STARTED,
+			Index: i,
+			InputFiles: []string{files[i]},
+		}
+		mapTasks = append(mapTasks, task)
+	}
+	c.mapTasks = mapTasks
+
+	var reduceTasks []Task
+	for j:=0; j<nReduce; j++{
+		var task = Task{
+			TaskType: REDUCE,
+			TaskStatus: NOT_STARTED,
+			Index: j,
+			InputFiles: getIntermediateFiles(j, len(files)),
+		}
+		reduceTasks = append(reduceTasks, task)
+	}
+	c.reduceTasks = reduceTasks
+
+	c.mapDone = false
+	c.reduceDone = false
 
 	c.server()
 	return &c
+}
+
+func getIntermediateFiles(j int, lenFiles int) []string{
+	var files []string
+	for i:=0; i<lenFiles; i++{
+		files = append(files, "mr-intermediate-"+strconv.Itoa(i)+"-"+strconv.Itoa(j))
+	}
+	return files
 }
--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -8,22 +8,45 @@ package mr
 
 import "os"
 import "strconv"
+import "time"
 
 //
 // example to show how to declare the arguments
 // and reply for an RPC.
 //
 
-type ExampleArgs struct {
-	X int
-}
+// type ExampleArgs struct {
+// 	X int
+// }
 
-type ExampleReply struct {
-	Y int
-}
+// type ExampleReply struct {
+// 	Y int
+// }
 
 // Add your RPC definitions here.
+type TaskStatus int
 
+const (
+	NOT_STARTED TaskStatus = iota
+	WAITING
+	DONE
+)
+
+type TaskType int
+
+const (
+	MAP TaskType = iota
+	REDUCE
+	BREAK
+)
+
+type Task struct {
+	TaskStatus TaskStatus
+	TaskType TaskType
+	InputFiles []string
+	Index int
+	TimeStamp time.Time
+}
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
